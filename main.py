7a3df9f462c44acb3cfd7714bee98b37ff1bc4dd
raka334 <readytogave@outlook.com>
2023-08-27 23:45:42 +0530

    added the basic working in main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 # This is the main file where the code will be added and we can easily contribute here 
+a = input("Enter your upi address")
+b = input ("Enter your price ")
+c=f"upi://pay?pa={a}&pn=Nupur&cu=INR&am={b}"
+print(f"upi://pay?pa={a}&pn=Nupur&cu=INR&am={b}")
