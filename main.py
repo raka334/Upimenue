a4b7c8e8b3de5a954615f25d05aec4c43b7e26bd
raka334 <readytogave@outlook.com>
2023-08-28 21:33:17 +0530

    added classes

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+def UpiLInkGenerator(upi_address="don't know yet", price=0, name="unknown"):    
 # This is the main file where the code will be added and we can easily contribute here 
-a = input("Enter your upi address")
-b = input ("Enter your price ")
-c= input("Enter your Name")
-print(f"upi://pay?pa={a}&pn={c}&cu=INR&am={b}")
+    address = upi_address
+    cost = price
+    nme = name
+    return f"upi://pay?pa={upi_address}&pn={nme}&cu=INR&am={cost}"
+
+
+if __name__ == "__main__":
+    link = UpiLInkGenerator(nupurkaushiknk@oksbi,600,"Nupar")
+    print(link)
